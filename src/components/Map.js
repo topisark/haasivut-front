24ebafd8341209f67864aff33a2a49fb3af0a2d5
topisark <topi.sarkkinen@gmail.com>
2020-01-12 12:01:58 +0200

    Walking path

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { GoogleMap, Marker, withGoogleMap } from 'react-google-maps'
+import { GoogleMap, Marker, Polyline, withGoogleMap } from 'react-google-maps'
 import InfoBox from 'react-google-maps/lib/components/addons/InfoBox'
 import PlaceMarker from './PlaceMarker'
 import heartIcon from '../assets/heart.png'
@@ -23,9 +23,150 @@ const markers = [
   }
 ]
 
+const walkingPath = [
+  {
+    "lat": 60.18455354722347,
+    "lng": 25.01082681900224
+  },
+  {
+    "lat": 60.18449425217528,
+    "lng": 25.011103363057256
+  },
+  {
+    "lat": 60.1844583921328,
+    "lng": 25.011147475549674
+  },
+  {
+    "lat": 60.18433494336332,
+    "lng": 25.011022065341535
+  },
+  {
+    "lat": 60.18435619529053,
+    "lng": 25.01089776190459
+  },
+  {
+    "lat": 60.18423679428914,
+    "lng": 25.010785188047475
+  },
+  {
+    "lat": 60.183855595591666,
+    "lng": 25.010467016689056
+  },
+  {
+    "lat": 60.18374410392334,
+    "lng": 25.01038956230423
+  },
+  {
+    "lat": 60.18320188242789,
+    "lng": 25.01001489431312
+  },
+  {
+    "lat": 60.18297516415075,
+    "lng": 25.00980715638474
+  },
+  {
+    "lat": 60.18266115347922,
+    "lng": 25.009560618735243
+  },
+  {
+    "lat": 60.1826638478517,
+    "lng": 25.009410857720855
+  },
+  {
+    "lat": 60.18265416742474,
+    "lng": 25.009231103564854
+  },
+  {
+    "lat": 60.18259179238865,
+    "lng": 25.008823134048445
+  },
+  {
+    "lat": 60.1824263151979,
+    "lng": 25.008435530092154
+  },
+  {
+    "lat": 60.182119830471784,
+    "lng": 25.008324911028883
+  },
+  {
+    "lat": 60.181904055634185,
+    "lng": 25.00833591149834
+  },
+  {
+    "lat": 60.181660416927365,
+    "lng": 25.008427557588934
+  },
+  {
+    "lat": 60.181536809160306,
+    "lng": 25.008437937589473
+  },
+  {
+    "lat": 60.181499072076456,
+    "lng": 25.00830155795525
+  },
+  {
+    "lat": 60.181534980236044,
+    "lng": 25.0079031035206
+  },
+  {
+    "lat": 60.18152379840883,
+    "lng": 25.00774770456046
+  },
+  {
+    "lat": 60.18146962354215,
+    "lng": 25.007416957173824
+  },
+  {
+    "lat": 60.18143850398294,
+    "lng": 25.007248739749272
+  },
+  {
+    "lat": 60.18132703786664,
+    "lng": 25.00727440624744
+  },
+  {
+    "lat": 60.18124491952808,
+    "lng": 25.007259773447068
+  },
+  {
+    "lat": 60.18117772490624,
+    "lng": 25.007149640885
+  },
+  {
+    "lat": 60.18092887571189,
+    "lng": 25.00672207788899
+  },
+  {
+    "lat": 60.18078857862028,
+    "lng": 25.00682270059356
+  },
+  {
+    "lat": 60.1807003795867,
+    "lng": 25.00688317970264
+  },
+  {
+    "lat": 60.18064675875214,
+    "lng": 25.006772111262794
+  },
+]
+
+const lineSymbol = {
+  path: 'M 0,-1 0,1',
+  strokeOpacity: 1,
+  scale: 4
+};
+
 const Map = () => {
+
+  const handleRightClick = event => {
+    const { lat, lng } = event.latLng
+    const coordinates = { lat: lat(), lng: lng() }
+    console.log(JSON.stringify(coordinates, null, 2) + ',')
+  }
+
   return (
     <GoogleMap
+      onRightClick={handleRightClick}
       defaultZoom={15}
       defaultCenter={{ lat: 60.18375031, lng: 25.0106028291 }}
       defaultOptions={{
@@ -43,6 +184,19 @@ const Map = () => {
         zoomControl: false,
       }}
     >
+      <Polyline
+        defaultOptions={{
+          icons: [{
+            icon: lineSymbol,
+            offset: '0',
+            repeat: '20px'
+          }],
+          strokeOpacity: 0,
+          strokeWeight: 2,
+          strokeColor: '#eec0c6'
+        }}
+        path={walkingPath}
+      />
       { markers.map(marker => (
         <Marker
           key={'marker-' + marker.label}
