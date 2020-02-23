acc9136474f3c4c0e4ef25a83f95854928931e4f
topisark <topi.sarkkinen@gmail.com>
2020-02-23 12:09:31 +0200

    Navbar styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,34 @@
 import React from 'react'
-import { BottomNavigation, BottomNavigationAction, Paper, withStyles } from '@material-ui/core'
+import { BottomNavigation, BottomNavigationAction, Paper, Typography, withStyles } from '@material-ui/core'
 import { Link, withRouter } from 'react-router-dom'
 
 const styles = theme => ({
+  root: {
+    display: 'flex',
+    alignItems: 'center'
+  },
   navigationRoot: {
+    flex: 1,
     backgroundColor: 'white'
   },
   navigationActionRoot: {
     color: '#7d797ad4'
-  }
+  },
+  header: {
+    fontFamily: 'Caveat, cursive',
+    fontSize: 20,
+    paddingLeft: 20,
+  },
 })
 
 const NavBar = ({ classes, pages, location }) => {
   const currentPath = location.pathname
 
   return (
-    <Paper id="navbar-root">
+    <Paper id="navbar-root" className={classes.root}>
+      <Typography className={classes.header} color="textPrimary">
+        Anniina & Topi
+      </Typography>
       <BottomNavigation
         classes={{ root: classes.navigationRoot }}
         value={ currentPath }
