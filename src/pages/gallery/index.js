4d7e9334e300669ed55b89f3da2743a5b9e486fc
topisark <topi.sarkkinen@gmail.com>
2020-06-22 07:37:39 +0300

    Lazyload

--- src/pages/gallery/index.js
+++ src/pages/gallery/index.js
@@ -39,7 +39,7 @@ const Gallery = ({ classes }) => {
 
   return (
     <div className={ classes.root }>
-      <ImageGallery items={ images }/>
+      <ImageGallery lazyLoad={true} items={ images }/>
     </div>
   )
 }
