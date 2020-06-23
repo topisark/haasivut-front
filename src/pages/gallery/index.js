79328a3aa3a1dfc88cb9d7170026b5dcd3f312b4
topisark <topi.sarkkinen@gmail.com>
2020-06-23 19:24:12 +0300

    Tabs

--- src/pages/gallery/index.js
+++ src/pages/gallery/index.js
@@ -1,4 +1,4 @@
-import { withStyles } from '@material-ui/core'
+import { withStyles, Tabs, Tab } from '@material-ui/core'
 import ImageGallery from 'react-image-gallery'
 import React from 'react'
 
@@ -8,10 +8,22 @@ const IMAGE_ROOT = 'https://topi-anniina-haakuvat.s3.eu-central-1.amazonaws.com'
 
 const IMAGE_AMOUNT = 29
 
+const styles = theme => ({
+  root: {
+    paddingTop: theme.spacing(2),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  tabsRoot: {
+    marginBottom: theme.spacing(2),
+  }
+})
+
 const renderItem = ({ original }) => {
   return (
     <div>
-      <img style={{ maxHeight: 'calc(100vh - 280px)' }} className="image-gallery-image" src={original}/>
+      <img style={{ maxHeight: 'calc(100vh - 320px)' }} className="image-gallery-image" src={original}/>
     </div>
   )
 }
@@ -22,17 +34,54 @@ const images = Array.from(Array(IMAGE_AMOUNT), (_, i) => i + 1).map(n => ({
   renderItem: renderItem
 }))
 
-const styles = theme => ({
-  root: {
-    paddingTop: theme.spacing(2),
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
 
-  },
+  return (
+    <div
+      style={{ display: value === index ? 'inherit' : 'none' }}
+      role="tabpanel"
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {children}
+    </div>
+  );
+}
+
+
+const Images = withStyles(styles)(({ classes }) => {
+  return (
+    <div style={{ maxWidth: 'calc(100vw - 20px)' }}>
+      <ImageGallery
+        lazyLoad={true}
+        items={ images }
+      />
+    </div>
+  )
 })
 
 const Gallery = ({ classes }) => {
+  const [ activeTab, setActiveTab ] = React.useState(0)
+
   return (
     <div className={ classes.root }>
-      <ImageGallery lazyLoad={true} items={ images }/>
+      <Tabs
+        value={activeTab}
+        onChange={(event, newValue) => setActiveTab(newValue)}
+        indicatorColor="primary"
+        classes={{ root: classes.tabsRoot }}
+      >
+        <Tab label="Häävideo"  />
+        <Tab label="Kuvagalleria" />
+      </Tabs>
+      <TabPanel value={activeTab} index={0}>
+        Video
+      </TabPanel>
+      <TabPanel value={activeTab} index={1}>
+        <Images/>
+      </TabPanel>
     </div>
   )
 }
